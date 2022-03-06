e698e490080d2760afaeb34e86c3d077d598e2b1
Luis Román <luisalroman94@gmail.com>
2022-03-06 09:15:54 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,32 +1,38 @@
-from datetime import date,datetime,timedelta
-import pandas as pd
-import numpy as np
-import requests, json
-
 class API():    
-    
     @staticmethod
-    def competi():
+    def get_competi():
         return 'mia_4'
     
     @staticmethod
-    def user_key():
-        return 'AIzaSyBIAVe1BIJaxb-LVyMYMmhtoPPdJZSfRqI'
+    def get_user_key():
+        return 'AIzaSyCWjvfiRkwssq-LX0Gwy6nsLfznrE44fuw'
     
     @staticmethod
-    def url_base():
+    def get_url_base():
         return 'https://miax-gateway-jog4ew3z3q-ew.a.run.app'
 
     @staticmethod
-    def ticker_master(market):
+    def get_ticker_master(market):
         
-        assert market in ["IBEX", "DAX", "EUROSTOXX"]
+        """
+          Retrieves the ticker master of a market.
+
+          Parameters
+          ----------
+              market (str) : market to retrieve the data from. It must be IBEX, DAX or EUROSTOXX.
+
+          Returns
+          --------
+              pd.DataFrame : the ticker master of the market.
+        """
         
-        url = f'{API.url_base()}/data/ticker_master'
+        assert market in ["IBEX", "DAX", "EUROSTOXX"], "Not a valid market. The market must be IBEX, DAX or EUROSTOXX"
         
-        params = {'competi': API.competi(),
+        url = f'{API.get_url_base()}/data/ticker_master'
+        
+        params = {'competi': API.get_competi(),
                   'market': market,
-                  'key': API.user_key()}
+                  'key': API.get_user_key()}
 
         response = requests.get(url, params)
         tk_master = response.json()
@@ -35,20 +41,59 @@ class API():
         
 
     @staticmethod
-    def historical_data(ticker, market):
+    def get_price_series(ticker, market):
+        """
+           Retrieves the price series of a stock.
 
+           Parameters
+           ----------
+               ticker (str) : stock to retrieve the data from.
+               market (str) : market to which the stock belongs. It must be IBEX, DAX or EUROSTOXX.
+
+           Returns
+           -------
+               pd.DataFrame : the ohlcv of the stock.
+        """
         
-        assert market in ["IBEX", "DAX", "EUROSTOXX"]
+        assert market in ["IBEX", "DAX", "EUROSTOXX"], "Not a valid market. The market must be IBEX, DAX or EUROSTOXX"
         
-        url = f'{API.url_base()}/data/time_series'
+        url = f'{API.get_url_base()}/data/time_series'
     
         params = {'market': market,
-                  'key': API.user_key(),
+                  'key': API.get_user_key(),
                   'ticker': ticker,
                   'close': False}
 
         response = requests.get(url, params)
         tk_data = response.json()
     
+        return pd.read_json(tk_data, typ='frame')
+    
+    @staticmethod
+    def get_benchmark_series(market):
+        """
+           Retrieves the price series of a benchmark.
+
+           Parameters
+           ----------
+               market (str) : market which corresponds to our benchmark. It must be IBEX, DAX or EUROSTOXX.
+               
+           Returns
+           -------
+               pd.DataFrame : the ohlcv of the benchmark.
+        """
+        
+        assert market in ["IBEX", "DAX", "EUROSTOXX"], "Not a valid market. The market must be IBEX, DAX or EUROSTOXX"
+        
+        url = f'{API.get_url_base()}/data/time_series'
+        
+        params = {'market':market,
+                'key':API.get_user_key(),
+                'ticker':'benchmark',
+                'close':False}
+        
+        response=requests.get(url, params)
+        tk_data=response.json()
+        
         return pd.read_json(tk_data, typ='frame')
     
